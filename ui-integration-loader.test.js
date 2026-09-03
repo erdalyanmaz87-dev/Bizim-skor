7cc6a162474808677be632575e9af2841b69ba1f
erdalyanmaz87-dev <erdalyanmaz87@gmail.com>
2026-09-03 10:11:28 +0300

    test: enforce safe UI integration script order

--- ui-integration-loader.test.js
+++ ui-integration-loader.test.js
@@ -3,10 +3,16 @@ const loader=require('./ui-integration-loader.js');
 assert.deepStrictEqual(loader.scriptOrder(),[
   'week-strip.js',
   'header-ui.js',
+  'home-prediction-priority.js',
   'horizontal-menu.js',
   'prediction-week-cards.js',
   'weekly-ranking-strip.js',
+  'fixture-data.js',
+  'fixture-ui.js',
   'fixture-week-strip.js',
   'history-week-strip.js'
 ]);
+const order=loader.scriptOrder();
+assert(order.indexOf('home-prediction-priority.js')<order.indexOf('prediction-week-cards.js'));
+assert(order.indexOf('fixture-ui.js')<order.indexOf('fixture-week-strip.js'));
 console.log('ui-integration-loader ok');
