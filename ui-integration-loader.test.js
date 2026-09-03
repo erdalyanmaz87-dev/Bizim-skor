1365e11f91afdc2209ae6b22192aad2e64239b89
erdalyanmaz87-dev <erdalyanmaz87@gmail.com>
2026-09-03 11:41:31 +0300

    test: include chat unread module in integration order

--- ui-integration-loader.test.js
+++ ui-integration-loader.test.js
@@ -5,6 +5,7 @@ assert.deepStrictEqual(loader.scriptOrder(),[
   'header-ui.js',
   'home-prediction-priority.js',
   'horizontal-menu.js',
+  'chat-unread-indicator.js',
   'prediction-week-cards.js',
   'weekly-ranking-strip.js',
   'fixture-data.js',
@@ -14,5 +15,6 @@ assert.deepStrictEqual(loader.scriptOrder(),[
 ]);
 const order=loader.scriptOrder();
 assert(order.indexOf('home-prediction-priority.js')<order.indexOf('prediction-week-cards.js'));
+assert(order.indexOf('horizontal-menu.js')<order.indexOf('chat-unread-indicator.js'));
 assert(order.indexOf('fixture-ui.js')<order.indexOf('fixture-week-strip.js'));
 console.log('ui-integration-loader ok');
