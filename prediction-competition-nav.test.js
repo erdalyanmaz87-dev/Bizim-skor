2f4567273375a9528413c623f6f1e0d94c1dac6e
erdalyanmaz87-dev <erdalyanmaz87@gmail.com>
2026-09-14 12:47:08 +0300

    test: match prediction selector markup structure

--- prediction-competition-nav.test.js
+++ prediction-competition-nav.test.js
@@ -3,11 +3,11 @@ const nav=require('./prediction-competition-nav.js');
 
 const superMarkup=nav.predictionCompetitionMarkup('pred');
 assert.match(superMarkup,/data-prediction-competition="pred"[^>]*aria-current="page"/);
-assert.match(superMarkup,/🇹🇷 Süper Lig/);
+assert.match(superMarkup,/>🇹🇷<\/span><b>Süper Lig<\/b>/);
 assert.match(superMarkup,/data-prediction-competition="championsPred"/);
-assert.match(superMarkup,/⭐ Şampiyonlar Ligi/);
+assert.match(superMarkup,/>⭐<\/span><b>Şampiyonlar Ligi<\/b>/);
 assert.match(superMarkup,/data-prediction-competition="nationsPred"/);
-assert.match(superMarkup,/🌍 Uluslar Ligi/);
+assert.match(superMarkup,/>🌍<\/span><b>Uluslar Ligi<\/b>/);
 
 const championsMarkup=nav.predictionCompetitionMarkup('championsPred');
 assert.match(championsMarkup,/data-prediction-competition="championsPred"[^>]*aria-current="page"/);
