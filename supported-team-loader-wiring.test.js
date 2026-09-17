f9d781cfd53b3a43f5e9f990919af5f04ec2d3ef
erdalyanmaz87-dev <erdalyanmaz87@gmail.com>
2026-09-17 12:51:53 +0300

    test: require isolated supported-team bootstrap

--- supported-team-loader-wiring.test.js
+++ supported-team-loader-wiring.test.js
@@ -1,8 +1,9 @@
 const fs=require('fs');
 const assert=require('assert');
-const html=fs.readFileSync('index.html','utf8');
 const theme=fs.readFileSync('theme-ui.js','utf8');
-const wiredDirectly=html.includes('<script src="ui-integration-loader.js" defer></script>');
-const wiredViaTheme=theme.includes("script.src='ui-integration-loader.js'")&&theme.includes('ensureIntegrationLoader');
-assert(wiredDirectly||wiredViaTheme,'UI entegrasyon loaderı gerçek sayfa yükleme zincirine bağlı olmalı');
-console.log('supported team loader wiring ok');
+assert(!theme.includes("script.src='ui-integration-loader.js'"),'theme genel UI loaderını yeniden başlatmamalı');
+for(const file of ['brand-assets.js','supported-team-ui.js','museum-team-logo-fix.js','admin-supported-team-stats.js']){
+  assert(theme.includes(`'${file}'`),`${file} izole takım bundle içinde yüklenmeli`);
+}
+assert(theme.includes('ensureSupportedTeamBundle'),'izole takım bundle bootstrapı bulunmalı');
+console.log('supported team isolated wiring ok');
