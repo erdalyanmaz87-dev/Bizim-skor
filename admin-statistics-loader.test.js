878fa3f13100af3e173e15fa5231eb6e9a0c6adf
erdalyanmaz87-dev <erdalyanmaz87@gmail.com>
2026-09-18 08:28:27 +0300

    test: load inactive admin extension after dashboard

--- admin-statistics-loader.test.js
+++ admin-statistics-loader.test.js
@@ -4,5 +4,7 @@ const src=fs.readFileSync('ui-integration-loader.js','utf8');
 assert.strictEqual(src.split('async function loadAll(').length-1,1);
 assert(src.includes('admin-statistics-utils.js'));
 assert(src.includes('admin-statistics-dashboard.js'));
+assert(src.includes('admin-inactive-21d.js'));
+assert(src.indexOf('admin-statistics-dashboard.js')<src.indexOf('admin-inactive-21d.js'));
 assert(src.includes('admin-statistics-dashboard.css'));
 console.log('admin statistics loader ok');
